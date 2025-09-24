95bf1939220341985831b4e574f3c649715fd2d4
raidelo <raidelosix@gmail.com>
2025-09-24 15:49:49 -0400

    initial commit
    Added initial commands to configure termux

---
+++ setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Grant storage access permissions to termux
+termux-setup-storage
+
+# Install some useful pakages
+pkg update
+pkg upgrade
+pkg install man
+pkg install neovim
+pkg install vim
+pkg install python
+pkg install nodejs
+pkg install wget
+pkg install git
+pkg install uv
+pkg install which
+pkg install perl
+pkg install ripgrep
+pkg install nmap
+
+# Set up some Git's configurations
+git config set --global init.defaultBranch main
+git config set --global user.email raidelosix@gmail.com
+git config set --global user.name raidelo
+git config set --global alias.lodag "log --oneline --decorate --all --graph"
+git config set --global alias.ll logdag
+git config set --global alias.co checkout
+git config set --global alias.ss status
+git config set --global core.editor nvim
+
+# Install some Python's useful libraries
+pip install requests
+pip install bs4
+pip install rich
+pip install colorama
+
+# Download of Neovim's config
+mkdir .config
+git clone https://github.com/raidelo/neovim-config.git ./.config/nvim
+
